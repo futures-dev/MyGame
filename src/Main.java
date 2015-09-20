82403396567306695fa81eec9be853c41aecf272
Andrei@Computer <futures_dev@outlook.com>
2015-09-20 18:18:34 +0300

    Initial Commit: Room, Fire/Spike/Block/Test Rooms, Exit, Lighter TODO: Help, Tutorial level, 2 more levels, troll, make lady walk.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 /**
  * Created by Андрей on 14.09.2015.
  */
+// chcp1251
 public class Main {
+    public static void main(String[] args){
+        Engine.Loader loader = new Engine.Loader();
+    }
 }
 
